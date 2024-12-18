6fdad8fe8a5df41b52ef194329511b5faa273f37
Mirjalol <miramirxomitov97@gmail.com>
2024-12-18 10:54:52 +0500

    added teacher slug

---
+++ pages/teacher/[slug].vue
@@ -0,0 +1,43 @@
+<script setup></script>
+<template>
+  <CBannerAllPage title="Distinguished Faculty Memebers" />
+  <div class="teacher-page py-[100px] 768:py-[70px]">
+    <div class="site-container">
+      <div class="grid grid-cols-2 gap-x-10 mb-14 768:grid-cols-1">
+        <div
+          class="text-[#192B69] text-5xl font-medium 768:mb-10 768:text-3xl 480:!text-xl 480:!mb-6"
+        >
+          Prof. Marvin McKinney
+        </div>
+        <div>
+          <div class="text-2xl font-medium mb-3 768:text-lg">
+            Head of Faculty of Information & Technology
+          </div>
+          <div class="mb-3">
+            Marvin brings a wealth of knowledge and experience to the Department
+            of Information Technology at our university. With a Ph.D.
+          </div>
+          <a href="mailto:marvin@gmail.com" class="text-[#648AC8]"
+            >Mail: marvin@gmail.com</a
+          >
+        </div>
+      </div>
+      <div class="grid grid-cols-2 gap-10 768:grid-cols-1 768:gap-6">
+        <div>
+          <img
+            src="https://framerusercontent.com/images/G8IPOia19ht2yFm8hCxawquAzE.png?scale-down-to=1024"
+            alt=""
+          />
+        </div>
+        <div>
+          Marvin brings a wealth of knowledge and experience to the Department
+          of Information Technology at our university. With a Ph.D. in Computer
+          Science from a prestigious institution and over a decade of teaching
+          and research experience, Dr. Thompson is a respected authority in the
+          field.
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss"></style>
