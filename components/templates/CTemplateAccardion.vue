6ae9ca1bee5a35cfe581acf42a29d7297dd8874c
Mirjalol <miramirxomitov97@gmail.com>
2025-01-23 10:53:40 +0500

    fix accardion for template 6

--- components/templates/CTemplateAccardion.vue
+++ components/templates/CTemplateAccardion.vue
@@ -17,11 +17,39 @@ const toggleAccordion = (index) => {
 
 <template>
   <div class="template">
-    <CBannerAllPage :title="content.title" />
+    <CBannerAllPage :title="content.title" :image="content.background" />
 
     <div class="site-container !py-[80px] 768:!py-[40px]">
       <div v-html="content?.description" class="mb-10"></div>
-      <div></div>
+      <div class="w-full mx-auto">
+        <div
+          v-for="(item, index) in content?.accordion_data"
+          :key="index"
+          class="mb-6 text-[#192B69]"
+        >
+          <div
+            class="text-xl font-medium flex-center justify-between cursor-pointer px-5 py-4 bg-[#F4F5F8]"
+            @click="toggleAccordion(index)"
+          >
+            <span class="mr-4">
+              {{ item.title }}
+            </span>
+            <img
+              :src="
+                activeIndex === index
+                  ? '/icons/accordion-minus2.svg'
+                  : '/icons/accordion-plus2.svg'
+              "
+              alt="toggle-icon"
+            />
+          </div>
+          <div
+            v-if="activeIndex === index"
+            class="mt-3 text-lg"
+            v-html="item.description"
+          ></div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
